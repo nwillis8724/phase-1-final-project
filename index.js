e30a047590db6d6b53469091261b71a4db7450b7
noah willis <n.willis.8724@gmail.com>
2022-11-29 21:49:28 -0600

    added a new clock

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-
 window.addEventListener('DOMContentLoaded', (event) => {
     console.log('DOM fully loaded');
 })
-
+$(document).ready(function() {
+    // Create two variables with names of months and days of the week in the array
+    var monthNames = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ]; 
+    var dayNames= ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+    
+    // Create an object newDate()
+    var newDate = new Date();
+    // Retrieve the current date from the Date object
+    newDate.setDate(newDate.getDate());
+    // At the output of the day, date, month and year    
+    $('#Date').html(dayNames[newDate.getDay()] + " " + newDate.getDate() + ' ' + monthNames[newDate.getMonth()] + ' ' + newDate.getFullYear());
+    
+    setInterval( function() {
+        // Create an object newDate () and extract the second of the current time
+        var seconds = new Date().getSeconds();
+        // Add a leading zero to the value of seconds
+        $("#sec").html(( seconds < 10 ? "0" : "" ) + seconds);
+        },1000);
+        
+    setInterval( function() {
+        // Create an object newDate () and extract the minutes of the current time
+        var minutes = new Date().getMinutes();
+        // Add a leading zero to the minutes
+        $("#min").html(( minutes < 10 ? "0" : "" ) + minutes);
+        },1000);
+        
+    setInterval( function() {
+        // Create an object newDate () and extract the clock from the current time
+        var hours = new Date().getHours();
+        // Add a leading zero to the value of hours
+        $("#hours").html(( hours < 10 ? "0" : "" ) + hours);
+        }, 1000);
+        
+    }); 
 //grab date and time
-let dt = new Date();
-document.getElementById("datetime").innerHTML = dt
 
 //create random number
 function getRandomInt(min, max) {
@@ -121,11 +151,7 @@ fetch('https://tasty.p.rapidapi.com/recipes/list?from=0&size=20&tags=under_30_mi
 	.catch(err => console.error(err));
 
 
-
-
-
-
-
+//clock date and time
 
 
   
